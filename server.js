01fdf6465c9ca0f13d6c40898e14a1565f3f0b53
Michael Pace <michael.g.pace@gmail.com>
2023-02-01 00:52:11 +0000

    put class configs in from class 7 repo

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 let data = require('./');
 
+const cors = require('cors')
 const app = express();
 app.use(cors());
 const Port = process.env.PORT || 5005;
@@ -10,3 +11,6 @@ app.get('/', (request, response)=>{
     response.send('Hello from our server HOME route / !!');
 });
 
+app.use('*', (request, response))
+
+app.listen(PORT, () => console.log('Listebibg on PORT ${PORT'));
