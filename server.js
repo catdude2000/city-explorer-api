144822732c16612867fb841fcc1b0c0a022077df
Michael Pace <michael.g.pace@gmail.com>
2023-02-01 00:58:26 +0000

    express server works, can see message in browser and terminal

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ let data = require('./');
 const cors = require('cors')
 const app = express();
 app.use(cors());
-const Port = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5005;
 
 app.get('/', (request, response)=>{
-    response.send('Hello from our server HOME route / !!');
+  response.send('Hello from our server HOME route / !!');
 });
 
-app.use('*', (request, response))
+// app.use('*', (request, response))
 
-app.listen(PORT, () => console.log('Listebibg on PORT ${PORT'));
+app.listen(PORT, () => console.log('Listening on PORT ${PORT)'));
