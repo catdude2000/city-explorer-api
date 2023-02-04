d6faa60b94f6d934d769dd9e7cd5e464e9740320
Michael Pace <michael.g.pace@gmail.com>
2023-02-04 15:47:59 -0600

    push for craig

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 require('dotenv').config();
 let data = require('./data/weather.json');
 const cors = require('cors');
+const { default: axios } = require('axios');
 // const axios = require('axios');
 
 const app = express();
@@ -20,21 +21,21 @@ app.get('/', (request, response) => {
 
 app.get('/weather', async (request, response) => {
   try {
-  // console.log(request.query, 'request');
-  // const weathUrl = `http://api.weatherbit.io/v2.0/forecast/daily?city=${}&key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
-  // let results = await axios.get(weathUrl)
+  console.log(request.query, 'request');
   
-
+  const weathUrl =  `http://api.weatherbit.io/v2.0/forecast/daily?city=Seattle&key=${WEATHER_API_KEY}`
+  // let results = await axios.get(weathUrl)
+    let weatherData = await axios.get(weathUrl)
   let lat = request.query.lat;
   let lon = request.query.lon;
   let searchQueryDone = request.query.searchQuery;
   console.log(searchQuery, 'searchquery');
   let cityLat = data.find(ele => ele.lat === lat);
   let cityLon = data.find(ele => ele.lon === lon);
-  let citySearch = data.find(ele => ele.searchQuery === searchQuery);
+  let citySearch = data.find(ele => ele.searchQueryDone === searchQueryDone);
   // let citySearch = data.find(ele => console.log(ele.city_name, 'ele'));
   // console.log(, 'citysearch');
-  let dataTosend = forecastObject.data.map(forecast => new Forecast(forecast));
+  let dataTosend = forecastObject.data.map(forecast => new Forecast(cityLat, cityLon, citySearch));
   response.status(200).send(dataTosend);
   } catch (error) {
     next (error);
