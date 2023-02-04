38e7255d79d4cc747fa7da9b9ea60ca511ffa1cb
Michael Pace <michael.g.pace@gmail.com>
2023-02-04 23:57:11 +0000

    gets request query from front

---
+++ weather.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+require('dotenv').config();
+const cors = require('cors');
+
+// module.exports = getWeather;
+
+// function mapWeather(weatherData) {
+//     try {
+//         const weatherSummaries = weatherData.
+//     }
+// }
