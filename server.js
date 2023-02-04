b289e908cf496ae526fae4895ee0985ed97cb18d
Michael Pace <michael.g.pace@gmail.com>
2023-02-04 04:07:39 +0000

    pause

--- server.js
+++ server.js
@@ -1,52 +1,83 @@
 'use strict';
+console.log('server is connected.');
+
 const express = require('express');
+
 require('dotenv').config();
 let data = require('./data/weather.json');
-const cors = require('cors')
-const axios = require('axios');
+
+const cors = require('cors');
+
+// const axios = require('axios');
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 5005;
-
-app.get('/', (request, response)=>{
-  response.send('Hello from our server HOME route / !!');
-});
 
 
+///////////////////////////////
+// let cache={}
+// async function getStuff(req, res, next){
+// if() {
 
-//////////////////////////////////
+// }else {
 
-app.get('/weather', (request, response) => {
-  try {
+//  }
+//  try {
+//   let searchQueryFromFrontEnd = req.query.searchQuery;
+//   let key = searchQueryFromFrontEnd = '-Data';
+//   let acceptableTimeTiCache = 1000 * 60 * 60 * 24 * 30;
+//   console.log(acceptableTimeTiCache, 'accepttimetocache');
+//   // let testTimeToCache = 1000 * 10;
 
+//   if(cache[key]   && Date.now() - cache[key]  ) {
+//     console.log('is in cache')
+//   }else {
+//     let exampurl = ***,
+//     let results = await axios.get(exampurl);
+//   };
+//   // console.log(cache, 'cache');
+//  } catch (error) {
 
-    let dataRequest = request.query.data;
-    let dataToInstantiate = data.find(weather => weather.dataRequest === dataRequest);
-    let dataToSend = new Forecast(dataToInstantiate);
-    console.log(dataToSend, 'got back');
-    response.status(200).send(dataToSend);
-  } catch (error) {
-    next(error);
-  }
-});
-
-
-// function weatherHandler(request, response) {
-
+//  }
 // }
+/////////////////////////////
 
-// class Forecast (date, description) => {
-//   constructor(forecastObject);
-//    {
-//     this.dataRequest = forecastObject.dataRequest;
-//   }
-// };
 
-/////////////////////////////////////////////////
+const PORT = process.env.PORT || 3002;
 
-app.use((error, request, response, next) => {
-  response.status(500).send(error.message);
+app.get('/', (request, response) => {
+  response.send('hello from the server.');
 });
+
+app.get('/weather', async (request, response) => {
+  console.log(request.query, 'request');
+  let lat = request.query.lat;
+  let lon = request.query.lon;
+  let searchQuery = request.query.searchQuery;
+  console.log(searchQuery, 'searchquery');
+  let cityLat = data.find(ele => ele.lat === lat);
+  let cityLon = data.find(ele => ele.lon === lon);
+  let citySearch = data.find(ele => ele.city_name === searchQuery);
+  // let citySearch = data.find(ele => console.log(ele.city_name, 'ele'));
+  // console.log(, 'citysearch');
+  response.send('/weather');
+  let dataTosend = new Forecast(citySearch, cityLon, cityLat);
+  response.send(dataTosend);
+  // let results = await axios.get(url)
+
+});
+//could be wrong url
+// const weathUrl = `http://api.weatherbit.io/v2.0/forecast/daily?city=Seattle&key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
+
+app.post();
+
+class Forecast{
+  constructor(forecastObject) {
+    this.datetime = data.datetime;
+    this.description = forecastObject.description;
+  }
+}
+
+
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
