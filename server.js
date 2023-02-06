790572300ac343549b1d5547c32582aefb614256
Michael Pace <michael.g.pace@gmail.com>
2023-02-06 23:13:19 +0000

    working on finding weather in json

--- server.js
+++ server.js
@@ -20,25 +20,26 @@ app.get('/', (request, response) => {
 
 app.get('/weather', async (request, response, next) => {
   try {
-    console.log(request.query.searchQuery, 'request');
+    // console.log(request.query.searchQuery, 'request');
 
     // const weathUrl =  `http://api.weatherbit.io/v2.0/forecast/daily?city=Seattle&key=${WEATHER_API_KEY}`
     // let results = await axios.get(weathUrl)
     // let weatherData = await axios.get(weathUrl)
     let lat = request.query.lat;
     console.log(lat, 'lat');
-    let lon = request.query.lon;
+    // let lon = request.query.lon;
     let searchQuery = request.query.searchQuery;
-    // console.log(searchQuery, 'searchquery');
+    console.log(searchQuery, 'searchquery');
     let cityLat = data.find(ele => ele.lat === lat);
-    let cityLon = data.find(ele => ele.lon === lon);
-    let citySearch = data.find(ele => ele.searchQuery === searchQuery);
-    console.log( citySearch, 'citylon');
+    console.log(cityLat, 'citylat');
+    // let cityLon = data.find(ele => ele.lon === lon);
+    let citySearch = data.find(ele => ele.city_name === searchQuery);
+    console.log( citySearch, 'citysearch');
     // let citySearch = data.find(ele => console.log(ele.city_name, 'ele'));
-    let dataTosend = new Forecast(cityLat, cityLon, citySearch);
-    console.log(dataTosend, 'datatosend');
+    // let dataTosend = new Forecast(cityLat, cityLon, searchQuery);
+    // console.log(dataTosend, 'datatosend');
     // forecastDataToSend.data.map(forecast => new Forecast(cityLat, cityLon, citySearch));
-    response.status(200).send(dataTosend);
+    // response.status(200).send(dataTosend);
   } catch (error) {
     next (error);
   }
@@ -48,11 +49,11 @@ app.get('*', (request, response) => {
   response.send('The route was not found. Error 404');
 });
 
-class Forecast{
-  constructor(forecastObject) {
-    this.datetime = forecastObject.datetime;
-    this.description = forecastObject.description;
-  }
-}
+// class Forecast{
+//   constructor(forecastObject) {
+//     this.date = forecastObject.date;
+//     this.description = forecastObject.description;
+//   }
+// }
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
