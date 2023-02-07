75aaf49f96a7d632b5a20e7606bc3e85d0835bac
Michael Pace <michael.g.pace@gmail.com>
2023-02-07 01:57:35 +0000

    7 done, code cleaned

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const express = require('express');
 require('dotenv').config();
 let data = require('./data/weather.json');
 const cors = require('cors');
-
-// const axios = require('axios');
-
 const app = express();
 app.use(cors());
 
@@ -17,25 +14,12 @@ app.get('/', (request, response) => {
   response.send('hello from the server.');
 });
 
-
 app.get('/weather', async (request, response, next) => {
   try {
-    // const weathUrl =  `http://api.weatherbit.io/v2.0/forecast/daily?city=Seattle&key=${WEATHER_API_KEY}`
-    // let results = await axios.get(weathUrl)
-    // let weatherData = await axios.get(weathUrl)
-    let lat = request.query.lat;
-    console.log(lat, 'lat');
-    // let lon = request.query.lon;
     let searchQuery = request.query.searchQuery;
-    console.log(searchQuery, 'searchquery');
-
     let weatherDataObject = data.find(ele => ele.city_name.toLocaleLowerCase() === searchQuery.toLocaleLowerCase());
-    console.log(weatherDataObject, 'weatherdataobj');
-    // let cityLat = data.find(ele => ele.lat === lat);
-
     let dataTosend = weatherDataObject.data.map(forecast => new Forecast(forecast));
-    console.log(dataTosend, 'datatosend');
-
+    // console.log(dataTosend, 'datatosend');
     response.status(200).send(dataTosend);
   } catch (error) {
     next (error);
