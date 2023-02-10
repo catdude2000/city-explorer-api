d4ebc84a47b61ecde11f457a115fbb7959202b2a
Michael Pace <michael.g.pace@gmail.com>
2023-02-10 01:04:25 +0000

    still working on weather api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 console.log('server is connected.');
-const axios = require('axios');
+const axios = require('axios').default;
 ////////^^^^^^correct?
 const express = require('express');
 require('dotenv').config();
@@ -11,7 +11,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
-// let W_API_KEY = process.env.WEATHER_API_KEY;
+let W_API_KEY = process.env.WEATHER_API_KEY;
 
 app.get('/', (request, response) => {
   response.send('hello from the server.');
@@ -20,30 +20,25 @@ app.get('/', (request, response) => {
 ////////////change from data to api info
 app.get('/weather', async (request, response, next) => {
   try {
-
-    let searchQuery= request.query.searchQuery;
-
+    // let searchQuery= request.query.searchQuery;
     let lat = request.query.lat;
     let lon= request.query.lon;
 
-    const url = `http://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${W_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
+    console.log(url, 'url');
     let weatherData = await axios.get(url);
-
-
-
-
     // let weatherDataObject = request.query.searchQuery.toLocaleLowerCase();
    
-
-    let weatherDataObject = weatherData.find(ele => ele.city_name.toLocaleLowerCase() === searchQuery);
-    console.log(weatherDataObject, 'weatherdtaobject');
+    // let weatherDataObject = weatherData.find(ele => ele.city_name.toLocaleLowerCase() === searchQuery);
+    // console.log(weatherDataObject, 'weatherdtaobject');
     ////^^^^^^^^^^^^^^^^replace?
 
-    let dataTosend = weatherDataObject.data.map(forecast => new Forecast(forecast));
-    // console.log(dataTosend, 'datatosend');
+    let dataTosend = weatherData.map(forecast => new Forecast(forecast));
+    console.log(dataTosend, 'datatosend');
     response.status(200).send(dataTosend);
   } catch (error) {
-    next (error);
+    next;
+    (error);
   }
 });
 ////////////////////
